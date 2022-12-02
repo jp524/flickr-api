938a8794b4e0f68aec5eb7e1118fd9511a4308c7
JP <85654561+jp524@users.noreply.github.com>
2022-12-02 17:56:17 -0500

    Display photos when valid user id is provided

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
+require 'flickr'
+
 class PagesController < ApplicationController
   def index
-    @flickr_user_id = pages_params[:flickr_user_id]
+    return unless params[:flickr_user_id]
+
+    flickr = Flickr.new ENV['flickr_api_key'], ENV['flickr_shared_secret']
+    @user_photos = flickr.photos.search user_id: params[:flickr_user_id]
   end
 
   private
