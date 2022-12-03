5a069a2204a3f8a02dcf60e8bb4894b826ba36a4
JP <85654561+jp524@users.noreply.github.com>
2022-12-02 20:52:25 -0500

    Add error handling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,22 @@ class PagesController < ApplicationController
     return unless params[:flickr_user_id]
 
     flickr = Flickr.new ENV['flickr_api_key'], ENV['flickr_shared_secret']
-    @user_photos = flickr.photos.search user_id: params[:flickr_user_id]
+
+    begin
+      @user_photos = flickr.photos.search user_id: params[:flickr_user_id]
+    rescue Flickr::FailedResponse => e
+      @error_msg = clean_error_msg(e.message)
+    end
   end
 
   private
 
   def pages_params
     params.permit(:flickr_user_id, :commit)
+  end
+
+  def clean_error_msg(msg)
+    msg.delete_prefix!("'flickr.photos.search' - ")
+    msg.prepend('Error: ')
   end
 end
